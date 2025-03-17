983075e2eb6d0f282e895d9726e6c1b6d16957a8
parthiv2806 <109407401+parthiv2806@users.noreply.github.com>
2025-03-17 16:05:33 +0530

    Update script.js

--- Rock-Paper-Scissors/script.js
+++ Rock-Paper-Scissors/script.js
@@ -1,27 +1,29 @@
 function playGame(playerchoice) {
-  const array = ["rock", "paper", "scissor"];
+  const array = ["rock", "paper", "scissors"]; 
   const computerChoice = array[Math.floor(Math.random() * 3)];
 
   console.log(`Player choice - ${playerchoice}`);
   console.log(`Computer choice - ${computerChoice}`);
 
   let result;
-  if (playerchoice == computerChoice) {
-    result = "Match is drow";
+  if (playerchoice === computerChoice) {
+    result = "Match is Draw"; 
   } else if (
     (playerchoice === "rock" && computerChoice === "scissors") ||
     (playerchoice === "scissors" && computerChoice === "paper") ||
     (playerchoice === "paper" && computerChoice === "rock")
   ) {
-    console.log("Congratulation you win");
-    result = "You win the mactch";
+    console.log("Congratulations, you win!");
+    result = "You win the match!"; 
   } else {
-    result = "You loss the game";
+    result = "You lost the game!"; 
   }
+
   document.getElementById(
     "result"
   ).innerText = `Player: ${playerchoice} | Computer: ${computerChoice}\n${result}`;
 }
+
 function resetGame() {
   document.getElementById("result").innerText = "";
 }
